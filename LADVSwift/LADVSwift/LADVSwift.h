7e3fe8906fc08f25296236ae41c24b9ff8fe3d20
Lukas Schmidt <lukas.schmi@gmail.com>
2017-04-16 11:36:37 +0200

    Project setup

---
+++ LADVSwift/LADVSwift/LADVSwift.h
@@ -0,0 +1,19 @@
+//
+//  LADVSwift.h
+//  LADVSwift
+//
+//  Created by Lukas Schmidt on 16.04.17.
+//  Copyright © 2017 freiraum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LADVSwift.
+FOUNDATION_EXPORT double LADVSwiftVersionNumber;
+
+//! Project version string for LADVSwift.
+FOUNDATION_EXPORT const unsigned char LADVSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LADVSwift/PublicHeader.h>
+
+
